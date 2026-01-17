858997ca30adc8a2752b159a0dd14180645d9659
Львов Сергей <lvov.sn03@gmail.com>
2026-01-17 12:06:53 +1000

    release 1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "ru.lsn03.voicemediacontroller"
         minSdk = 30
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.0.0"
+        versionCode = 3
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
