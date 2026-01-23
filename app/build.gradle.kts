d7aaad4ac1baab2d0788d31c17536d7495de5fad
Львов Сергей <lvov.sn03@gmail.com>
2026-01-23 11:56:37 +1000

    bump 1.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "ru.lsn03.voicemediacontroller"
         minSdk = 30
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.0.2"
+        versionCode = 5
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
